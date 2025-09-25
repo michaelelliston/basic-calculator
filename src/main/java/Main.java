2c14e0664209c607f6b8f841b50579cfa92949d8
Michael <michael.elliston95@gmail.com>
2025-09-25 10:27:44 -0500

    final commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        String input = myScanner.nextLine();
+
+        double firstNumber = Double.parseDouble(input);
+
+        System.out.println("Enter the second number:");
+        String inputTwo = myScanner.nextLine();
+
+        double secondNumber = Double.parseDouble(inputTwo);
+
+        System.out.println("The answer is: " + firstNumber * secondNumber + '.');
+
+    }
 }
